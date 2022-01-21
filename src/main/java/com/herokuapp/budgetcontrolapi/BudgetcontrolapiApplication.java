8525275b7c37dbb64057d0335ffb323194ed107d
Tiago Boaventura Miranda <98120297+tgobm@users.noreply.github.com>
2022-01-21 19:48:47 -0300

    feat: spring project initial commit

---
+++ src/main/java/com/herokuapp/budgetcontrolapi/BudgetcontrolapiApplication.java
@@ -0,0 +1,13 @@
+package com.herokuapp.budgetcontrolapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetcontrolapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetcontrolapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/herokuapp/budgetcontrolapi/BudgetcontrolapiApplicationTests.java
@@ -0,0 +1,14 @@
+package com.herokuapp.budgetcontrolapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetcontrolapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+        BudgetcontrolapiApplication.main(new String[]{});
+    }
+
+}
